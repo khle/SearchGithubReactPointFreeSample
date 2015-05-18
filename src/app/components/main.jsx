f9afb0d727579c6fb9ecf2f93b4cea58480f57f0
pragmaticlogic <pragmaticlogic@gmail.com>
2015-05-18 03:34:30 +0000

    improved by making more functional

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -21,24 +21,43 @@ var Main = React.createClass({
             var onSearchClick = ((event) => { 
                 
                 //Impure
-                var noop = element => { return (() => { element.value = ''; element.focus() }) }
-                var showResult = element => { return (() => Navigate(`/dashboard/${element.value}`)) }
-                var element = this.refs.searchText.getDOMNode()
+                var element = this.refs.searchText.getDOMNode()                
+                var focusElement = element => { element.value = ''; element.focus() }                
+                var showResult = element => { Navigate(`/dashboard/${element.value}`) }
                                                                                                 
                 //Pure
                 var getInput = element => element.value
                 var isInputEmpty = R.eq('')
                 var checkInput = R.compose(isInputEmpty, R.trim, getInput)
-                var handler = R.ifElse(checkInput, noop, showResult)
-                                
-                                
-                handler(element)()
+                var handleClick = R.ifElse(checkInput, focusElement, showResult)
+                      
+                                                
+                handleClick(element)
                 
             }).bind(context)  
             
-            var onKeyPressHandler = ((event) => {
+            var onKeyDownHandler = ((event) => {
                 
-            })
+                //Impure
+                var element = this.refs.searchText.getDOMNode()
+                var showResult = value => { Navigate(`/dashboard/${value}`) }                                
+                
+                //Pure
+                var doNotShowAnything = val => { return }
+                var isInputEmpty = R.eq('')
+                var checkInput = R.compose(isInputEmpty, R.trim)
+                var search = R.ifElse(checkInput, doNotShowAnything, showResult)                
+                var getKeyPressed = event => event.keyCode
+                var handleNonEnterKeyPressed = evt => val => { return }
+                var handleEnterKeyPressed = evt => val => { search(val) } //impure
+                var isEnterPressed = R.eq(13)
+                var checkEnterKeyPressed = R.compose(isEnterPressed, getKeyPressed)                
+                var handleKeyPressed = R.ifElse(checkEnterKeyPressed, handleEnterKeyPressed, handleNonEnterKeyPressed)
+                
+                
+                handleKeyPressed(event)(element.value)                
+                
+            }).bind(context)
                                 
             var displayUserName = R.ifElse(R.eq(null), x => '', R.identity)
             var displayError = R.ifElse(R.eq(null), x => '', x => ' user not found.')
@@ -47,7 +66,7 @@ var Main = React.createClass({
                 
                 <div className="row">
                     <div className="input-field col s12 m8 offset-m2">
-                        <input placeholder="Github username" id="search-text" type="text" className="validate" value={context.state.userName} onChange={onKeyPressHandler} ref="searchText" />
+                        <input placeholder="Github username" id="search-text" type="text" className="validate" value={context.state.userName} onKeyDown={onKeyDownHandler} ref="searchText" />
                         <label className="active" for="search-label">Search Github user</label>
                     </div>
                 </div>
